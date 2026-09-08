1fc138b694484cdd1bcb1c74e6eb4825687fefac
PatriciaGea <patricia.gea@gmail.com>
2026-09-08 23:41:08 +0200

    feat: add Google Credential Manager helper for ID token retrieval
    Wraps CredentialManager and GetGoogleIdOption to fetch a Google ID
    token that can be exchanged for a Firebase credential.

--- app/src/main/java/se/tattooink/powerfy/ui/components/GoogleAuthHelper.kt
+++ app/src/main/java/se/tattooink/powerfy/ui/components/GoogleAuthHelper.kt
@@ -1,2 +1,30 @@
 package se.tattooink.powerfy.ui.components
 
+import android.content.Context
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialException
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import se.tattooink.powerfy.BuildConfig
+
+suspend fun requestGoogleIdToken(context: Context): Result<String> {
+    return try {
+        val option = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(BuildConfig.GOOGLE_WEB_CLIENT_ID)
+            .build()
+
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(option)
+            .build()
+
+        val credentialManager = CredentialManager.create(context)
+        val result = credentialManager.getCredential(context, request)
+
+        val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(result.credential.data)
+        Result.success(googleIdTokenCredential.idToken)
+    } catch (e: GetCredentialException) {
+        Result.failure(e)
+    }
+}
