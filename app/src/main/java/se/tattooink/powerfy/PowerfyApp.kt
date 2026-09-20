dd18b4749402676c51ec90f077fc2490c98cc3d6
PatriciaGea <patricia.gea@gmail.com>
2026-09-20 20:13:13 +0200

    feat: initialize Stripe payment configuration on app launch
    Ensures PaymentConfiguration is set up as early as possible in the
    application lifecycle, before any payment sheet is presented.

--- app/src/main/java/se/tattooink/powerfy/PowerfyApp.kt
+++ app/src/main/java/se/tattooink/powerfy/PowerfyApp.kt
@@ -1,7 +1,17 @@
 package se.tattooink.powerfy
 
 import android.app.Application
+import com.stripe.android.PaymentConfiguration
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PowerfyApp : Application()
+class PowerfyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        PaymentConfiguration.init(applicationContext, BuildConfig.STRIPE_PUBLISHABLE_KEY)
+    }
+}
+
+
+
+
